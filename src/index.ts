d4a8dc9ba773c25c4018cd5c8a5a9fb5ebbe2b9a
ahdg <36436808+ahdg6@users.noreply.github.com>
2023-08-07 15:47:23 +0800

    v0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-import { Context, Schema } from 'koishi'
-import {confChecker, gcChecker} from './spark';
+import { Context, Schema } from "koishi";
+import { confChecker, gcChecker } from "./spark";
 import fs from "fs";
 import path from "path";
 
-export const name = 'mcdev'
-export const using = ['puppeteer']
+export const name = "mcdev";
+export const using = ["puppeteer"];
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  const chotaStyles = fs.readFileSync(path.join(__dirname, 'chota.min.css'), 'utf-8');
+  const chotaStyles = fs.readFileSync(
+    path.join(__dirname, "chota.min.css"),
+    "utf-8",
+  );
 
-  ctx.command('spark <url:strings>').action(async ({ session }, url) => {
-    if (!url) return '请输入网址。'
-    if (!url.startsWith('https://spark')) return '不是 spark 报告的网址。'
-    const response_raw = await fetch(url + '?raw=1')
-    const sampler: any = await response_raw.json().catch(() => undefined)
-    if (!sampler) return '获取数据失败，请检查是不是 spark 报告的链接。'
+  ctx.command("spark <url:strings>").action(async ({ session }, url) => {
+    if (!url) return "请输入网址。";
+    if (!url.startsWith("https://spark")) return "不是 spark 报告的网址。";
+    const response_raw = await fetch(url + "?raw=1");
+    const sampler: any = await response_raw.json().catch(() => undefined);
+    if (!sampler) return "获取数据失败，请检查是不是 spark 报告的链接。";
 
-    const cards: { name: string; value: string; }[] = []
+    const cards: { name: string; value: string }[] = [];
 
-    const platform = sampler.metadata.platform.version
-    const user = sampler.metadata.user.name
-    const configs = sampler.metadata.serverConfigurations
-    let isServer: boolean = true
-    if (!configs) isServer = false
-    const plugins: { name: string; version: string }[] = Object.values(
-      sampler.metadata.sources
-    )
-    const flags = sampler.metadata.systemStatistics.java.vmArgs
-    const versionString = sampler.metadata.systemStatistics.java.vendorVersion
-    const majorVersion = parseInt(versionString.split('.')[0], 10)
+    const configs = sampler.metadata.serverConfigurations;
+    let isServer: boolean = true;
+    if (!configs) isServer = false;
+    const flags = sampler.metadata.systemStatistics.java.vmArgs;
+    const versionString = sampler.metadata.systemStatistics.java.vendorVersion;
+    const majorVersion = parseInt(versionString.split(".")[0], 10);
 
-    let cardHTML = '';
+    let cardHTML = "";
     await Promise.all([
       confChecker(configs).then((fields) => cards.push(...fields)),
-      gcChecker(flags, isServer, majorVersion).then((field) => cards.unshift(field))
+      gcChecker(flags, isServer, majorVersion).then((field) =>
+        cards.unshift(field),
+      ),
     ]).then(() => {
       for (const card of cards) {
         cardHTML += `
@@ -48,14 +48,14 @@ export function apply(ctx: Context) {
       </div>
     `;
       }
-    })
+    });
 
     return `<html>
     <head>
       <style>
         ${chotaStyles}
         body {
-          background: linear-gradient(to right, #f2f3f7, #e2e9f3);
+          background: linear-gradient(to right, #e9f1fc, #c0cfe3);
         }
         .card {
           box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%), 0 0 0 1px rgb(10 10 10 / 2%);
@@ -82,10 +82,10 @@ export function apply(ctx: Context) {
     <body>
       <div class="col-12">
          <div class="card">
-            <h2>Spark - ${url.substring(url.lastIndexOf('\/') + 1)}</h2>
-            <p>${platform} - ${user}</p>
-            <div class="tag">本项目开源于 GitHub ahdg6/koishi-plugin-mcdev</div>
-            <div class="tag">功能代码借鉴了 Discord bot - CraftyAssistant</div>
+            <h2>Spark - ${url.substring(url.lastIndexOf("/") + 1)}</h2>
+            <p>本项目开源于 GitHub ahdg6/koishi-plugin-mcdev，Discord 版本请搜索 CraftyAssistant</p>
+            <div class="tag">平台：${sampler.metadata.platform.version}</div>
+            <div class="tag">上传用户：${sampler.metadata.user.name}</div>
          </div>
       </div>
       <div class="container">
@@ -94,6 +94,6 @@ export function apply(ctx: Context) {
         </div>
       </div>
     </body>
-    </html>`
-  })
+    </html>`;
+  });
 }
--- src/spark.ts
+++ src/spark.ts
@@ -1,153 +1,171 @@
-import path from 'path'
-import yaml from 'js-yaml'
+import path from "path";
 import fs from "fs";
-import jexl from 'jexl';
+import jexl from "jexl";
 
 interface Checker {
-  expressions?: string[]
-  prefix: string
-  value: string
+  expressions?: string[];
+  prefix: string;
+  value: string;
 }
 
 const CHECKCONF = {
-  bukkit: yaml.load(fs.readFileSync(path.join(__dirname, 'analysis_config', 'bukkit.yml'), 'utf8')),
-  spigot: yaml.load(fs.readFileSync(path.join(__dirname, 'analysis_config', 'spigot.yml'), 'utf8')),
-  server_properties: yaml.load(fs.readFileSync(path.join(__dirname, 'analysis_config', 'server.properties.yml'), 'utf8')),
-  purpur: yaml.load(fs.readFileSync(path.join(__dirname, 'analysis_config', 'purpur.yml'), 'utf8')),
-  paper: yaml.load(fs.readFileSync(path.join(__dirname, 'analysis_config', 'paper.yml'), 'utf8')),
-}
-export async function confChecker(configs){
-  const fields: { name: string; value: string; }[] = []
+  bukkit: JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "analysis_config", "bukkit.json"),
+      "utf8",
+    ),
+  ),
+  spigot: JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "analysis_config", "spigot.json"),
+      "utf8",
+    ),
+  ),
+  server_properties: JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "analysis_config", "server.properties.json"),
+      "utf8",
+    ),
+  ),
+  purpur: JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "analysis_config", "purpur.json"),
+      "utf8",
+    ),
+  ),
+  paper: JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "analysis_config", "paper.json"),
+      "utf8",
+    ),
+  ),
+};
+export async function confChecker(configs) {
+  const fields: { name: string; value: string }[] = [];
 
-  let variablesMap = {}
+  let variablesMap = {};
 
   const configKeys = [
-    ['server.properties', 'server_properties'],
-    ['bukkit.yml', 'bukkit'],
-    ['spigot.yml', 'spigot'],
-    ['paper/', 'paper'],
-    ['purpur.yml', 'purpur']
-  ]
+    ["server.properties", "server_properties"],
+    ["bukkit.yml", "bukkit"],
+    ["spigot.yml", "spigot"],
+    ["paper/", "paper"],
+    ["purpur.yml", "purpur"],
+  ];
 
   configKeys.forEach(([key, varName]) => {
     if (configs[key]) {
-      variablesMap[varName] = JSON.parse(configs[key])
+      variablesMap[varName] = JSON.parse(configs[key]);
     }
-  })
+  });
 
   for (const name in variablesMap) {
-    const configName = `config.${name}`
-    const configObj: any = await CHECKCONF[name]
-    if (!configObj) continue
+    const configName = `config.${name}`;
+    const configObj: any = await CHECKCONF[name];
+    if (!configObj) continue;
     for (const nodePath in configObj) {
-      const checkArray: Checker[] = configObj[nodePath]
+      const checkArray: Checker[] = configObj[nodePath];
       for (let i = 0; i < checkArray.length; i++) {
-        let expressions = checkArray[i].expressions
+        let expressions = checkArray[i].expressions;
         // @ts-ignore
-        const allExpressionsTrue = expressions.every(
-          async (expressionStr) => {
-            try {
-              const result = await jexl.eval(expressionStr, variablesMap)
-              return !!result
-            } catch (error) {
-              fields.push(errorField(nodePath, error))
-              return false
-            }
+        const allExpressionsTrue = expressions.every(async (expressionStr) => {
+          try {
+            const result = await jexl.eval(expressionStr, variablesMap);
+            return !!result;
+          } catch (error) {
+            fields.push(errorField(nodePath, error));
+            return false;
           }
-        )
+        });
         if (allExpressionsTrue)
-          fields.push(createField(nodePath, checkArray[i]))
+          fields.push(createField(nodePath, checkArray[i]));
       }
     }
   }
 
-  return fields
+  return fields;
 }
 
 export async function gcChecker(
   jvmFlagsString: string,
   isServer: boolean,
-  jvmVersion: number
+  jvmVersion: number,
 ) {
   function extractMemoryAndGcType(
-    jvmFlagString: string
+    jvmFlagString: string,
   ): [number | null, string | null] {
-    const regex = /-Xm[sx]([0-9]+[kmg])\b.*?(-XX:\+Use(\w+)GC)\b/gi
-    const matches = regex.exec(jvmFlagString)
+    const regex = /-Xm[sx]([0-9]+[kmg])\b.*?(-XX:\+Use(\w+)GC)\b/gi;
+    const matches = regex.exec(jvmFlagString);
     if (matches && matches.length > 3) {
-      const memorySizeStr = matches[1]
-      const gcType = matches[3]
+      const memorySizeStr = matches[1];
+      const gcType = matches[3];
 
-      const memorySize = parseMemorySize(memorySizeStr)
+      const memorySize = parseMemorySize(memorySizeStr);
 
-      return [memorySize, gcType]
+      return [memorySize, gcType];
     }
 
-    return [null, null]
+    return [null, null];
   }
   function parseMemorySize(memorySizeStr: string): number | null {
-    const size = parseInt(memorySizeStr, 10)
+    const size = parseInt(memorySizeStr, 10);
     if (!isNaN(size)) {
-      if (memorySizeStr.endsWith('g')) {
-        return size * 1024 // GB 转换为 MB
-      } else if (memorySizeStr.endsWith('k')) {
-        return size / 1024 // KB 转换为 MB
+      if (memorySizeStr.endsWith("g")) {
+        return size * 1024; // GB 转换为 MB
+      } else if (memorySizeStr.endsWith("k")) {
+        return size / 1024; // KB 转换为 MB
       } else {
-        return size // MB
+        return size; // MB
       }
     }
-    return null
+    return null;
   }
-  const [memorySize, gcType] = extractMemoryAndGcType(jvmFlagsString)
+  const [memorySize, gcType] = extractMemoryAndGcType(jvmFlagsString);
   if (memorySize == null || gcType == null)
     return {
-      name: '⚠️ Flags',
-      value: 'We can not analyse your flags.'
-    }
-  if (gcType == 'Z' && memorySize <= 20480) {
+      name: "⚠️ 标签",
+      value: "我们无法对你的启动标签进行扫描。",
+    };
+  if (gcType == "Z" && memorySize <= 20480) {
     return {
-      name: '❗ ZGC',
-      value: `ZGC is known to be usable when you allocated 20GB+ Memory
-        , But you only allocated ${memorySize}MB so increase it or change GC (Use /mcflags to generate one).`
-    }
+      name: "❗ ZGC",
+      value: `ZGC 应仅在分配 20GB+ 内存时使用，但你只分配 ${memorySize}MB。`,
+    };
   }
-  if (gcType == 'Shenandoah' && isServer) {
+  if (gcType == "Shenandoah" && isServer) {
     return {
-      name: '❗ Shenandoah',
-      value: `ShenandoahGC is **Not** server friendly,
-        It only works well on client side. Use our /mcflags to generate better one.`
-    }
+      name: "❗ Shenandoah",
+      value: `ShenandoahGC 在服务端上表现欠佳，推荐仅在客户端使用`,
+    };
   }
-  if (gcType == 'G1') {
+  if (gcType == "G1") {
     if (memorySize >= 20480 && jvmVersion >= 16) {
       return {
-        name: '❗ G1 to ZGC',
-        value: `You are allocating 20GB+ in Java${jvmVersion}
-            , I would like to recommend you hava a try with ZGC as It will greatly improve your GC stop time.`
-      }
+        name: "❗ G1 to ZGC",
+        value: `你在 Java${jvmVersion} 上分配了 20GB+ 的内存，因此 ZGC 是个不错的选择，请考虑更换。`,
+      };
     }
     if (
       memorySize >= 12088 &&
-      jvmFlagsString.includes('-XX:G1NewSizePercent=30')
+      jvmFlagsString.includes("-XX:G1NewSizePercent=30")
     )
       return {
-        name: '❗ G1 Improvement',
-        value: `When you allocated 12GB+ memory
-        in G1GC, Please consider changing some flags value. (Use /mcflags to generate)`
-      }
+        name: "❗ G1 改进",
+        value: `当你分配 12GB+ 内存并使用 G1GC，你可以更改一些值来增进性能。`,
+      };
   }
   return {
     name: `✅ ${gcType}GC`,
-    value: 'Good job. We can not find any problems in your flags.'
-  }
+    value: "好样的，你的启动标签在自动扫描中没有任何问题。",
+  };
 }
 
 function createField(node: string, option: Checker) {
-  const field = { name: node, value: option.value }
-  if (option.prefix) field.name = option.prefix + ' ' + field.name
-  return field
+  const field = { name: node, value: option.value };
+  if (option.prefix) field.name = option.prefix + " " + field.name;
+  return field;
 }
 
 function errorField(node: string, error: unknown) {
-  return { name: '⚠️' + node, value: String(error) }
+  return { name: "⚠️" + node, value: String(error) };
 }
